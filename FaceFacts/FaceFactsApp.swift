3f1901f6482e11d5a1d4d8a60fbdac6340e16c8d
Boris Romao Antunes <romaoboris@gmail.com>
2023-12-25 05:52:25 -0300

    Initial Commit

---
+++ FaceFacts/FaceFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceFactsApp.swift
+//  FaceFacts
+//
+//  Created by Boris Romão Antunes on 25/12/23.
+//
+
+import SwiftUI
+
+@main
+struct FaceFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
